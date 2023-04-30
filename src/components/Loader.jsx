47f1569a3ed932b79101f7da842a5b55a9ab82e9
Tyson Brown <90238566+tysonpbr@users.noreply.github.com>
2023-04-30 16:34:28 -0700

    updated loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -14,16 +14,6 @@ const CanvasLoader = () => {
       }}
     >
       <span className='canvas-loader'></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
     </Html>
   );
 };
